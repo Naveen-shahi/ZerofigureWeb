759803e99fa9a94abddab80f03dd6fe5e4b68a47
Naveen-shahi <39208525+Naveen-shahi@users.noreply.github.com>
2022-01-26 20:58:31 +0530

    Add files via upload
    all changes

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,75 @@
 
 //for select Subscription
-$('.selectSubscription .heading').on('click',function(){
-  $(this).parents('.selectSubscription').addClass('open');
+$('.customSelectTag .heading').on('click', function () {
+   $(this).parents('.customSelectTag').addClass('open');
 });
-$('.selectSubscription li').on('click',function(){
-   $(this).parents('.selectSubscription').find('.heading').text($(this).attr('data-value'));
-   $(this).parents('.selectSubscription').removeClass('open');
+$('.customSelectTag li').on('click', function () {
+   $(this).parents('.customSelectTag').find('.heading').text($(this).attr('data-value'));
+   $(this).parents('.customSelectTag').removeClass('open').addClass('nowSelected');
    console.log($(this).attr('data-value'));
 })
 // for tabs
-$('.tabHeader li a').on('click',function(){
+$('.tabHeader li a').on('click', function () {
    $('.tabHeader li a').removeClass('active');
    $(this).addClass('active')
    $('.tabBody div').removeClass('active');
-   $('.tabBody '+$(this).attr('data-href')).addClass('active');
+   $('.tabBody ' + $(this).attr('data-href')).addClass('active');
 })
 //for Edit my detail
-$('.editBtn').on('click',function(){
+$('.editBtn').on('click', function () {
    $(this).parents('tr').find('input').prop('readonly', false).focus();
 })
+//for frame Name Edit
+$('.frameName .pencilIcon').on('click', function () {
+   $(this).parents('.frameName').addClass('editMode');
+})
+//for Active frame modal 
+$('.activeFrame').on('click', function () {
+   $('body').addClass('frameActiveOpen');
+})
+$('.frameModal .closeIcon,.overlay').on('click', function () {
+   $('body').removeClass('frameActiveOpen');
+})
 //for modal create account & sing in
-$('.myaccountBtn').on('click',function(){
+$('.myaccountBtn').on('click', function () {
    $('body').addClass('openSingIn')
-}) 
+})
+// for Enter tag
+var myValues = [];
+// var i = myValues.length;
+$(document).on('keypress', function (e) {
+   if (e.which == 13) {
+      appendChips();
+   }
+});
+function appendChips() {
+   if ($('#enterTags').val()) {
+      $('.makeTags input').before(`
+      <span class="chips"><span class="text">${$('#enterTags').val()}</span> <i class="delIcon"></i></span>
+    `)
+      // i++;
+      myValues.push($('#enterTags').val());
+      $('.makeTags input').val('')
+      $('.makeTags input').focus();
+      console.log(myValues);
+   }
+}
+// for delete Tags
+$(document).on('click', '.makeTags i.delIcon', function () {
+   console.log($(this).parent().attr('data-index'), 'ff')
+   $(this).parent().remove();
+   myValues.splice(myValues.indexOf($(this).parent().find('.text').text()),1);
+   console.log(myValues)
+})
+// .on('keyup', function () {
+
+// })
 //for password field 
 $('i.passwordShowBtn').on('click', function () {
-    $(this).toggleClass("eyeOpen");
-    if ($(this).parents('.form-group').find("input").attr("type") === "password") {
-       $(this).parents('.form-group').find("input").attr("type", "text");
-    } else {
-       $(this).parents('.form-group').find("input").attr("type", "password");
-    }  
- });
+   $(this).toggleClass("eyeOpen");
+   if ($(this).parents('.form-group').find("input").attr("type") === "password") {
+      $(this).parents('.form-group').find("input").attr("type", "text");
+   } else {
+      $(this).parents('.form-group').find("input").attr("type", "password");
+   }
+});
